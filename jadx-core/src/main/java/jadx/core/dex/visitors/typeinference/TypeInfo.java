6f18a04f02a79b84f0df6bd281c6620abea7fcd9
Pascal-Joos <pascal.joos@outlook.de>
2025-12-04 13:47:34 +0000

    Revert "fix: 92 - RETURN_NULLABLE - return type; - returning @Nullable expression from method with @NonNull return type"
    This reverts commit 80c25a1b307980ee2761cc2b2cbbb25fa32ea5ac.

--- jadx-core/src/main/java/jadx/core/dex/visitors/typeinference/TypeInfo.java
+++ jadx-core/src/main/java/jadx/core/dex/visitors/typeinference/TypeInfo.java
@@ -10,7 +10,7 @@ import org.jetbrains.annotations.NotNull;
 import jadx.core.dex.instructions.args.ArgType;
 
 public class TypeInfo {
-
+	@Nullable
 	private ArgType type = ArgType.UNKNOWN;
 
 	private final Set<ITypeBound> bounds = new LinkedHashSet<>();
@@ -21,7 +21,7 @@ public class TypeInfo {
 	}
 
 	public void setType(@Nullable ArgType type) {
-		this.type = type != null ? type : ArgType.UNKNOWN;
+		this.type = type;
 	}
 
 	public Set<ITypeBound> getBounds() {
