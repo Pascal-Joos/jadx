a8323158cfe38f8c33065f9eb7415ab0265bba41
Pascal-Joos <pascal.joos@outlook.de>
2025-12-04 12:43:20 +0000

    fix: 71 - DEREFERENCE_NULLABLE - insn.add(AFlag.TRY_LEAVE); - dereferenced expression insn is @Nullable

--- jadx-core/src/main/java/jadx/core/dex/visitors/AttachTryCatchVisitor.java
+++ jadx-core/src/main/java/jadx/core/dex/visitors/AttachTryCatchVisitor.java
@@ -86,6 +86,9 @@ public class AttachTryCatchVisitor extends AbstractVisitor {
 			}
 		}
 		if (tryBlockStarted) {
+			if (insn == null) {
+				throw new IllegalStateException("TRY_ENTER set but insn is null");
+			}
 			insn.add(AFlag.TRY_LEAVE);
 		} else {
 			// no instructions found in range -> add nop at start offset
