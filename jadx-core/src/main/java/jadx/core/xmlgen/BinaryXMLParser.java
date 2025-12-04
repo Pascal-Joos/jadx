0890f0622f67f34e7defbce12b92062c23528624
Pascal-Joos <pascal.joos@outlook.de>
2025-12-04 14:17:16 +0000

    Revert "fix: 106 - ASSIGN_FIELD_NULLABLE - nsMap = null; - assigning @Nullable expression to @NonNull field"
    This reverts commit fc2ea3aa8a013655c16b7cc36a9888d68e342593.

--- jadx-core/src/main/java/jadx/core/xmlgen/BinaryXMLParser.java
+++ jadx-core/src/main/java/jadx/core/xmlgen/BinaryXMLParser.java
@@ -83,6 +83,7 @@ public class BinaryXMLParser extends CommonBinaryParser {
 		writer.add("<?xml version=\"1.0\" encoding=\"utf-8\"?>");
 		firstElement = true;
 		decode();
+		nsMap = null;
 		ICodeInfo codeInfo = writer.finish();
 		this.classNameCache = null; // reset class name cache
 		return codeInfo;
