86ce97194d9d00fa2642c01b90babfca006e9933
Pascal-Joos <pascal.joos@outlook.de>
2025-12-04 12:09:33 +0000

    Revert "fix: 59 - ASSIGN_FIELD_NULLABLE - this.compare = null; - assigning @Nullable expression to @NonNull field"
    This reverts commit 810e64fb01d1b63696293e7e71d9742a3b3054da.

--- jadx-core/src/main/java/jadx/core/dex/regions/conditions/IfCondition.java
+++ jadx-core/src/main/java/jadx/core/dex/regions/conditions/IfCondition.java
@@ -36,7 +36,6 @@ public final class IfCondition extends AttrNode {
 
 	private final Mode mode;
 	private final List<IfCondition> args;
-	@Nullable
 	private final Compare compare;
 
 	private IfCondition(Compare compare) {
