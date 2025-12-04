512e400aab4ab827ee1d09bdd7accff40ad58156
Pascal-Joos <pascal.joos@outlook.de>
2025-12-04 12:31:50 +0000

    fix: 66 - DEREFERENCE_NULLABLE - List<InsnNode> insns = preCondition.getInstructions(); - dereferenced expression preCondition is @Nullable

--- jadx-core/src/main/java/jadx/core/dex/regions/loops/LoopRegion.java
+++ jadx-core/src/main/java/jadx/core/dex/regions/loops/LoopRegion.java
@@ -78,6 +78,10 @@ public final class LoopRegion extends ConditionRegion {
 	 * Check if pre-conditions can be inlined into loop condition
 	 */
 	public boolean checkPreCondition() {
+		BlockNode preCondition = this.preCondition;
+		if (preCondition == null) {
+			return false;
+		}
 		List<InsnNode> insns = preCondition.getInstructions();
 		if (insns.isEmpty()) {
 			return true;
