dd2681245125738db75bee92b3f6ca3eb8d23630
Pascal-Joos <pascal.joos@outlook.de>
2025-12-04 09:57:41 +0000

    Revert "fix: 16 - DEREFERENCE_NULLABLE - mapping.getClasses().add(jsonCls); - dereferenced expression mapping.getClasses() is @Nullable"
    This reverts commit bc4e3b857e181d1f72dd53c261c209d8926bcb10.

--- jadx-core/src/main/java/jadx/core/codegen/json/JsonMappingGen.java
+++ jadx-core/src/main/java/jadx/core/codegen/json/JsonMappingGen.java
@@ -9,6 +9,7 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
@@ -18,7 +19,6 @@ import jadx.core.codegen.json.mapping.JsonFieldMapping;
 import jadx.core.codegen.json.mapping.JsonMapping;
 import jadx.core.codegen.json.mapping.JsonMthMapping;
 import jadx.core.dex.info.ClassInfo;
-import jadx.core.dex.info.FieldInfo;
 import jadx.core.dex.info.MethodInfo;
 import jadx.core.dex.nodes.ClassNode;
 import jadx.core.dex.nodes.FieldNode;
@@ -28,15 +28,15 @@ import jadx.core.utils.exceptions.JadxRuntimeException;
 import jadx.core.utils.files.FileUtils;
 
 public class JsonMappingGen {
-	public static void dump(RootNode root) {
-		save(root);
-	}
-
 	private static final Logger LOG = LoggerFactory.getLogger(JsonMappingGen.class);
 
-	private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
+	private static final Gson GSON = new GsonBuilder()
+			.setPrettyPrinting()
+			.setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_DASHES)
+			.disableHtmlEscaping()
+			.create();
 
-	public static void save(RootNode root) {
+	public static void dump(RootNode root) {
 		JsonMapping mapping = new JsonMapping();
 		fillMapping(mapping, root);
 
@@ -54,7 +54,7 @@ public class JsonMappingGen {
 
 	private static void fillMapping(JsonMapping mapping, RootNode root) {
 		List<ClassNode> classes = root.getClasses(true);
-		List<JsonClsMapping> jsonClasses = new ArrayList<>(classes.size());
+		mapping.setClasses(new ArrayList<>(classes.size()));
 		for (ClassNode cls : classes) {
 			ClassInfo classInfo = cls.getClassInfo();
 			JsonClsMapping jsonCls = new JsonClsMapping();
@@ -67,9 +67,8 @@ public class JsonMappingGen {
 			}
 			addFields(cls, jsonCls);
 			addMethods(cls, jsonCls);
-			jsonClasses.add(jsonCls);
+			mapping.getClasses().add(jsonCls);
 		}
-		mapping.setClasses(jsonClasses);
 	}
 
 	private static void addMethods(ClassNode cls, JsonClsMapping jsonCls) {
@@ -97,9 +96,8 @@ public class JsonMappingGen {
 		jsonCls.setFields(new ArrayList<>(fields.size()));
 		for (FieldNode field : fields) {
 			JsonFieldMapping jsonField = new JsonFieldMapping();
-			FieldInfo fieldInfo = field.getFieldInfo();
-			jsonField.setName(fieldInfo.getName());
-			jsonField.setAlias(fieldInfo.getAlias());
+			jsonField.setName(field.getName());
+			jsonField.setAlias(field.getAlias());
 			jsonCls.getFields().add(jsonField);
 		}
 	}
