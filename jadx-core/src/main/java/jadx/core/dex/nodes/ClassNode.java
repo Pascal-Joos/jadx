261bf9452279b658b89d3768a7917feea454bd8d
Pascal-Joos <pascal.joos@outlook.de>
2025-12-04 11:24:33 +0000

    Revert "fix: 44 - METHOD_NO_INIT - public ClassNode(RootNode root, IClassData cls) { - initializer method does not guarantee @NonNull field parentClass (line 81) is initialized along all control-flow paths (remember to check for exceptions or early returns)."
    This reverts commit 4d1aa7bd1bc114003f50bb6ff28fbef441234926.

--- jadx-core/src/main/java/jadx/core/dex/nodes/ClassNode.java
+++ jadx-core/src/main/java/jadx/core/dex/nodes/ClassNode.java
@@ -104,7 +104,6 @@ public class ClassNode extends NotificationAttrNode implements ILoadable, ICodeN
 	private Map<MethodInfo, MethodNode> mthInfoMap = Collections.emptyMap();
 
 	public ClassNode(RootNode root, IClassData cls) {
-		this.parentClass = this;
 		this.root = root;
 		this.clsInfo = ClassInfo.fromType(root, ArgType.object(cls.getType()));
 		this.clsData = cls.copy();
