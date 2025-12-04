3da318f7cd7d9bb66852c8c13170102085fb16a3
Pascal-Joos <pascal.joos@outlook.de>
2025-12-04 14:32:33 +0000

    Revert "fix: 111 - DEREFERENCE_NULLABLE - String origKeyName = pkg.getKeyStrings()[key]; - dereferenced expression pkg.getKeyStrings() is @Nullable"
    This reverts commit 3b3dab5d9081ae5331bfd89909d043ed84758150.

--- jadx-core/src/main/java/jadx/core/xmlgen/ResTableParser.java
+++ jadx-core/src/main/java/jadx/core/xmlgen/ResTableParser.java
@@ -300,17 +300,8 @@ public class ResTableParser extends CommonBinaryParser implements IResParser {
 		}
 
 		int resRef = pkg.getId() << 24 | typeId << 16 | entryId;
-
 		String typeName = pkg.getTypeStrings()[typeId - 1];
-
-		String[] keyStrings = pkg.getKeyStrings();
-
-		String origKeyName = keyStrings != null && key >= 0 && key < keyStrings.length
-
-				? keyStrings[key]
-
-				: ("res_" + Integer.toHexString(resRef));
-
+		String origKeyName = pkg.getKeyStrings()[key];
 		ResourceEntry newResEntry = new ResourceEntry(resRef, pkg.getName(), typeName, getResName(typeName, resRef, origKeyName), config);
 		ResourceEntry prevResEntry = resStorage.searchEntryWithSameName(newResEntry);
 		if (prevResEntry != null) {
