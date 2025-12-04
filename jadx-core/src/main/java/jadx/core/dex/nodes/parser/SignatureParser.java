d80a90125e54f9aa91ac781f2c3314d3b9d52c80
Pascal-Joos <pascal.joos@outlook.de>
2025-12-04 12:02:41 +0000

    fix: 56 - RETURN_NULLABLE - return null; - returning @Nullable expression from method with @NonNull return type

--- jadx-core/src/main/java/jadx/core/dex/nodes/parser/SignatureParser.java
+++ jadx-core/src/main/java/jadx/core/dex/nodes/parser/SignatureParser.java
@@ -156,7 +156,7 @@ public class SignatureParser {
 				return ArgType.array(consumeType());
 
 			case STOP_CHAR:
-				return null;
+				throw new JadxRuntimeException("Unexpected end of signature: " + debugString());
 
 			default:
 				// primitive type (one char)
