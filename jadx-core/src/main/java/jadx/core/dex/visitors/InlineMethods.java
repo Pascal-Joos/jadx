98bdfb3f6be1d28913b3d4a3c9b032b20b2125c1
Pascal-Joos <pascal.joos@outlook.de>
2025-12-04 12:59:41 +0000

    Revert "fix: 75 - DEREFERENCE_NULLABLE - InsnNode inlCopy = mia.getInsn().copyWithoutResult(); - dereferenced expression mia.getInsn() is @Nullable"
    This reverts commit bb0c2df54436adb238d96b757747394f3e9df314.

--- jadx-core/src/main/java/jadx/core/dex/visitors/InlineMethods.java
+++ jadx-core/src/main/java/jadx/core/dex/visitors/InlineMethods.java
@@ -78,18 +78,18 @@ public class InlineMethods extends AbstractVisitor {
 	}
 
 	private void inlineMethod(MethodNode mth, MethodNode callMth, MethodInlineAttr mia, BlockNode block, InvokeNode insn) {
-		InsnNode inlInsn = java.util.Objects.requireNonNull(mia.getInsn());
-		InsnNode inlCopy = inlInsn.copyWithoutResult();
+		InsnNode inlCopy = mia.getInsn().copyWithoutResult();
 		RegisterArg resultArg = insn.getResult();
 		if (resultArg != null) {
 			inlCopy.setResult(resultArg.duplicate());
-		} else if (isAssignNeeded(inlInsn, insn, callMth)) {
+		} else if (isAssignNeeded(mia.getInsn(), insn, callMth)) {
 			// add fake result to make correct java expression (see test TestGetterInlineNegative)
 			inlCopy.setResult(makeFakeArg(mth, callMth.getReturnType(), "unused"));
 		}
 		if (!callMth.getMethodInfo().getArgumentsTypes().isEmpty()) {
+			// remap args
 			InsnArg[] regs = new InsnArg[callMth.getRegsCount()];
-			int[] regNums = java.util.Objects.requireNonNull(mia.getArgsRegNums());
+			int[] regNums = mia.getArgsRegNums();
 			for (int i = 0; i < regNums.length; i++) {
 				InsnArg arg = insn.getArg(i);
 				regs[regNums[i]] = arg;
