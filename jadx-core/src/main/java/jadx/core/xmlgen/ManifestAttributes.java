b10e804ac276b6492b7f3a24a3a266c33c19aa48
Pascal-Joos <pascal.joos@outlook.de>
2025-12-04 14:22:39 +0000

    Revert "fix: 108 - RETURN_NULLABLE - return instance; - returning @Nullable expression from method with @NonNull return type"
    This reverts commit 80914a7624c371b6d3c197aea1283d86f91901ac.

--- jadx-core/src/main/java/jadx/core/xmlgen/ManifestAttributes.java
+++ jadx-core/src/main/java/jadx/core/xmlgen/ManifestAttributes.java
@@ -62,7 +62,6 @@ public class ManifestAttributes {
 				instance = new ManifestAttributes();
 			} catch (Exception e) {
 				LOG.error("Failed to create ManifestAttributes", e);
-				throw new JadxRuntimeException("Failed to create ManifestAttributes", e);
 			}
 		}
 		return instance;
