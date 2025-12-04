8bbae9ff65c921353db4b354f52526c87ea2b880
Pascal-Joos <pascal.joos@outlook.de>
2025-12-04 09:59:47 +0000

    fix: 17 - DEREFERENCE_NULLABLE - return inputFilePath.getParent().resolve(baseName + ".jobf"); - dereferenced expression inputFilePath.getParent() is @Nullable

--- jadx-core/src/main/java/jadx/core/deobf/DeobfPresets.java
+++ jadx-core/src/main/java/jadx/core/deobf/DeobfPresets.java
@@ -55,7 +55,8 @@ public class DeobfPresets {
 		}
 		Path inputFilePath = jadxArgs.getInputFiles().get(0).toPath().toAbsolutePath();
 		String baseName = FileUtils.getPathBaseName(inputFilePath);
-		return inputFilePath.getParent().resolve(baseName + ".jobf");
+		Path parent = inputFilePath.getParent();
+		return (parent != null ? parent : inputFilePath).resolve(baseName + ".jobf");
 	}
 
 	private DeobfPresets(Path deobfMapFile) {
