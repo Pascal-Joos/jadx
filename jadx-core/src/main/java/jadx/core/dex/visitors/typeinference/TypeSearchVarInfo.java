f236fadc1a6ac7b4fbdc3db31667eeb546280363
Pascal-Joos <pascal.joos@outlook.de>
2025-12-08 17:44:03 +0000

    fix: Type='RETURN_NULLABLE', message='returning @Nullable expression from method with @NonNull return type' /home/vscode/nullness-benchmarks/jadx/jadx-core/src/main/java/jadx/core/dex/visitors/typeinference/TypeSearchVarInfo.java:85 		return constraints;

--- jadx-core/src/main/java/jadx/core/dex/visitors/typeinference/TypeSearchVarInfo.java
+++ jadx-core/src/main/java/jadx/core/dex/visitors/typeinference/TypeSearchVarInfo.java
@@ -3,8 +3,6 @@ package jadx.core.dex.visitors.typeinference;
 import java.util.Collections;
 import java.util.List;
 
-import javax.annotation.Nullable;
-
 import com.uber.nullaway.annotations.Initializer;
 
 import jadx.core.dex.instructions.args.ArgType;
@@ -16,8 +14,7 @@ public class TypeSearchVarInfo {
 	private ArgType currentType;
 	private List<ArgType> candidateTypes;
 	private int currentIndex = -1;
-	@Nullable
-	private List<ITypeConstraint> constraints;
+	private List<ITypeConstraint> constraints = Collections.emptyList();
 
 	public TypeSearchVarInfo(SSAVar var) {
 		this.var = var;
