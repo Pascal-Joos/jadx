99c49198d598a74324cf807de5e328e8e8711518
Pascal-Joos <pascal.joos@outlook.de>
2025-12-08 14:49:07 +0000

    fix: Type='METHOD_NO_INIT', message='initializer method does not guarantee @NonNull field arrayData (line 13) is initialized along all control-flow paths (remember to check for exceptions or early returns).' /home/vscode/nullness-benchmarks/jadx/jadx-core/src/main/java/jadx/core/dex/instructions/FillArrayInsn.java:15 	public FillArrayInsn(InsnArg arg, int target) {

--- jadx-core/src/main/java/jadx/core/dex/instructions/FillArrayInsn.java
+++ jadx-core/src/main/java/jadx/core/dex/instructions/FillArrayInsn.java
@@ -3,6 +3,8 @@ package jadx.core.dex.instructions;
 import java.util.List;
 import java.util.Objects;
 
+import javax.annotation.Nullable;
+
 import jadx.core.dex.instructions.args.ArgType;
 import jadx.core.dex.instructions.args.InsnArg;
 import jadx.core.dex.instructions.args.LiteralArg;
@@ -10,6 +12,7 @@ import jadx.core.dex.nodes.InsnNode;
 
 public final class FillArrayInsn extends InsnNode {
 	private final int target;
+	@Nullable
 	private FillArrayData arrayData;
 
 	public FillArrayInsn(InsnArg arg, int target) {
@@ -50,18 +53,25 @@ public final class FillArrayInsn extends InsnNode {
 	}
 
 	public int getSize() {
-		return arrayData.getSize();
+		return getArrayData().getSize();
 	}
 
 	public ArgType getElementType() {
-		return arrayData.getElementType();
+		return getArrayData().getElementType();
 	}
 
 	public List<LiteralArg> getLiteralArgs(ArgType elType) {
-		return arrayData.getLiteralArgs(elType);
+		return getArrayData().getLiteralArgs(elType);
 	}
 
 	public String dataToString() {
-		return Objects.toString(arrayData);
+		return Objects.toString(getArrayData());
+	}
+
+	private FillArrayData getArrayData() {
+		if (arrayData == null) {
+			throw new IllegalStateException("Array data not initialized");
+		}
+		return arrayData;
 	}
 }
