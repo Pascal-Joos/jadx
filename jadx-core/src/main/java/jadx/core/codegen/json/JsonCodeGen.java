e0c37fcc4ccd08eab1d254be292e0656da84c22d
Pascal-Joos <pascal.joos@outlook.de>
2025-12-08 14:22:53 +0000

    fix: Type='DEREFERENCE_NULLABLE', message='dereferenced expression jsonCls.getMethods() is @Nullable' /home/vscode/nullness-benchmarks/jadx/jadx-core/src/main/java/jadx/core/codegen/json/JsonCodeGen.java:161 			jsonCls.getMethods().add(jsonMth);

--- jadx-core/src/main/java/jadx/core/codegen/json/JsonCodeGen.java
+++ jadx-core/src/main/java/jadx/core/codegen/json/JsonCodeGen.java
@@ -139,7 +139,8 @@ public class JsonCodeGen {
 	}
 
 	private void addMethods(ClassNode cls, JsonClass jsonCls, ClassGen classGen) {
-		jsonCls.setMethods(new ArrayList<>());
+		List<JsonMethod> methods = new ArrayList<>();
+		jsonCls.setMethods(methods);
 		for (MethodNode mth : cls.getMethods()) {
 			if (mth.contains(AFlag.DONT_GENERATE)) {
 				continue;
@@ -160,7 +161,7 @@ public class JsonCodeGen {
 			jsonMth.setAccessFlags(mth.getAccessFlags().rawValue());
 			jsonMth.setLines(fillMthCode(mth, mthGen));
 			jsonMth.setOffset("0x" + Long.toHexString(mth.getMethodCodeOffset()));
-			jsonCls.getMethods().add(jsonMth);
+			methods.add(jsonMth);
 		}
 	}
 
