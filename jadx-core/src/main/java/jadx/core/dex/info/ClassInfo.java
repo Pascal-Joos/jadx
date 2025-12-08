47867fac8ab1014b310cb6a3067bb321869e5b34
Pascal-Joos <pascal.joos@outlook.de>
2025-12-08 14:46:09 +0000

    fix: Type='RETURN_NULLABLE', message='returning @Nullable expression from method with @NonNull return type' /home/vscode/nullness-benchmarks/jadx/jadx-core/src/main/java/jadx/core/dex/info/ClassInfo.java:92 		return alias == null ? getPackage() : alias.getPkg();

--- jadx-core/src/main/java/jadx/core/dex/info/ClassInfo.java
+++ jadx-core/src/main/java/jadx/core/dex/info/ClassInfo.java
@@ -171,7 +171,7 @@ public final class ClassInfo implements Comparable<ClassInfo> {
 			}
 			return parentFullName + innerSep + shortName;
 		}
-		return pkg.isEmpty() ? shortName : pkg + '.' + shortName;
+		return (pkg == null || pkg.isEmpty()) ? shortName : pkg + '.' + shortName;
 	}
 
 	private String makeFullName() {
