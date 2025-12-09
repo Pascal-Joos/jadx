b1f0943fc42e5a1ba3effbdec9d4a2943a7e0e9b
Pascal-Joos <pascal.joos@outlook.de>
2025-12-09 15:10:11 +0000

    Revert "fix: 1 - METHOD_NO_INIT - public ClsSet(RootNode root) { - initializer method does not guarantee @NonNull field classes (line 75) is initialized along all control-flow paths (remember to check for exceptions or early returns)."
    This reverts commit 33953d5301cbddf0a14f4697b208b4b7f291f5cd.

--- jadx-core/src/main/java/jadx/core/clsp/ClsSet.java
+++ jadx-core/src/main/java/jadx/core/clsp/ClsSet.java
@@ -27,8 +27,6 @@ import org.jetbrains.annotations.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import edu.ucr.cs.riple.annotator.util.Nullability;
-
 import jadx.api.plugins.utils.ZipSecurity;
 import jadx.core.dex.info.AccessInfo;
 import jadx.core.dex.info.ClassInfo;
@@ -74,7 +72,6 @@ public class ClsSet {
 		PRIMITIVE
 	}
 
-	@Nullable
 	private ClspClass[] classes;
 
 	public void loadFromClstFile() throws IOException, DecodeException {
@@ -85,10 +82,10 @@ public class ClsSet {
 			}
 			load(input);
 		}
-		if (classes != null && LOG.isDebugEnabled()) {
+		if (LOG.isDebugEnabled()) {
 			long time = System.currentTimeMillis() - startTime;
 			int methodsCount = Stream.of(classes).mapToInt(clspClass -> clspClass.getMethodsMap().size()).sum();
-			LOG.debug("Clst file loaded in {}ms, classes: {}, methods: {}", time, Nullability.castToNonnull(classes).length, methodsCount);
+			LOG.debug("Clst file loaded in {}ms, classes: {}, methods: {}", time, classes.length, methodsCount);
 		}
 	}
 
@@ -213,22 +210,18 @@ public class ClsSet {
 	}
 
 	private void save(OutputStream output) throws IOException {
-		if (classes == null) {
-			return;
-		}
 		DataOutputStream out = new DataOutputStream(output);
 		out.writeBytes(JADX_CLS_SET_HEADER);
 		out.writeByte(VERSION);
 
-		ClspClass[] nonNullClasses = Nullability.castToNonnull(classes);
-		Map<String, ClspClass> names = new HashMap<>(nonNullClasses.length);
-		out.writeInt(nonNullClasses.length);
-		for (ClspClass cls : nonNullClasses) {
+		Map<String, ClspClass> names = new HashMap<>(classes.length);
+		out.writeInt(classes.length);
+		for (ClspClass cls : classes) {
 			String clsName = cls.getName();
 			writeString(out, clsName);
 			names.put(clsName, cls);
 		}
-		for (ClspClass cls : nonNullClasses) {
+		for (ClspClass cls : classes) {
 			writeArgTypesArray(out, cls.getParents(), names);
 			writeArgTypesList(out, cls.getTypeParameters(), names);
 			List<ClspMethod> methods = cls.getSortedMethodsList();
@@ -237,8 +230,8 @@ public class ClsSet {
 				writeMethod(out, method, names);
 			}
 		}
-		int methodsCount = Stream.of(nonNullClasses).mapToInt(c -> c.getMethodsMap().size()).sum();
-		LOG.info("Classes: {}, methods: {}, file size: {} bytes", nonNullClasses.length, methodsCount, out.size());
+		int methodsCount = Stream.of(classes).mapToInt(c -> c.getMethodsMap().size()).sum();
+		LOG.info("Classes: {}, methods: {}, file size: {} bytes", classes.length, methodsCount, out.size());
 	}
 
 	private static void writeMethod(DataOutputStream out, ClspMethod method, Map<String, ClspClass> names) throws IOException {
@@ -429,9 +422,6 @@ public class ClsSet {
 		if (ordinal >= TypeEnum.values().length) {
 			throw new JadxRuntimeException("Incorrect ordinal for type enum: " + ordinal);
 		}
-		if (classes == null) {
-			throw new JadxRuntimeException("Classes not loaded");
-		}
 		switch (TypeEnum.values()[ordinal]) {
 			case WILDCARD:
 				ArgType.WildcardBound bound = ArgType.WildcardBound.getByNum(in.readByte());
@@ -447,8 +437,7 @@ public class ClsSet {
 				return ArgType.outerGeneric(outerType, innerType);
 
 			case GENERIC:
-				ClspClass[] localClasses = classes;
-				ArgType clsType = Nullability.castToNonnull(localClasses[in.readInt()].getClsType());
+				ArgType clsType = classes[in.readInt()].getClsType();
 				return ArgType.generic(clsType, readArgTypesList(in));
 
 			case GENERIC_TYPE_VARIABLE:
@@ -457,8 +446,7 @@ public class ClsSet {
 				return ArgType.genericType(typeVar, extendTypes);
 
 			case OBJECT:
-				ClspClass[] localClassesObj = classes;
-				return localClassesObj[in.readInt()].getClsType();
+				return classes[in.readInt()].getClsType();
 
 			case ARRAY:
 				return ArgType.array(readArgType(in));
@@ -513,16 +501,10 @@ public class ClsSet {
 	}
 
 	public int getClassesCount() {
-		if (classes == null) {
-			return 0;
-		}
-		return Nullability.castToNonnull(classes).length;
+		return classes.length;
 	}
 
 	public void addToMap(Map<String, ClspClass> nameMap) {
-		if (classes == null) {
-			return;
-		}
 		for (ClspClass cls : classes) {
 			nameMap.put(cls.getName(), cls);
 		}
