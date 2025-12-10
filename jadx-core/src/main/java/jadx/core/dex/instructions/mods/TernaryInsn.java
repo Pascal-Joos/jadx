80913a3f14baac33492c48d3e7590250f57894e4
Pascal-Joos <pascal.joos@outlook.de>
2025-12-10 11:30:07 +0000

    Revert "fix: 40 - ASSIGN_FIELD_NULLABLE - this.condition = condition; - assigning @Nullable expression to @NonNull field"
    This reverts commit 25333c43bcd1f10f1528205b01aac4101d11d726.

--- jadx-core/src/main/java/jadx/core/dex/instructions/mods/TernaryInsn.java
+++ jadx-core/src/main/java/jadx/core/dex/instructions/mods/TernaryInsn.java
@@ -5,8 +5,6 @@ import java.util.function.Consumer;
 
 import javax.annotation.Nullable;
 
-import edu.ucr.cs.riple.annotator.util.Nullability;
-
 import jadx.core.dex.instructions.InsnType;
 import jadx.core.dex.instructions.args.InsnArg;
 import jadx.core.dex.instructions.args.RegisterArg;
@@ -16,7 +14,6 @@ import jadx.core.utils.InsnUtils;
 
 public final class TernaryInsn extends InsnNode {
 
-	@Nullable
 	private IfCondition condition;
 
 	public TernaryInsn(@Nullable IfCondition condition, @Nullable RegisterArg result, InsnArg th, InsnArg els) {
@@ -40,14 +37,13 @@ public final class TernaryInsn extends InsnNode {
 		super(InsnType.TERNARY, 2);
 	}
 
-	@Nullable
 	public IfCondition getCondition() {
 		return condition;
 	}
 
 	public void simplifyCondition() {
 		condition = IfCondition.simplify(condition);
-		if (condition != null && condition.getMode() == IfCondition.Mode.NOT) {
+		if (condition.getMode() == IfCondition.Mode.NOT) {
 			invert();
 		}
 	}
@@ -62,17 +58,12 @@ public final class TernaryInsn extends InsnNode {
 	@Override
 	public void getRegisterArgs(Collection<RegisterArg> list) {
 		super.getRegisterArgs(list);
-		IfCondition cond = condition;
-		if (cond != null) {
-			list.addAll(Nullability.castToNonnull(condition.getRegisterArgs()));
-		}
+		list.addAll(condition.getRegisterArgs());
 	}
 
 	public void visitInsns(Consumer<InsnNode> visitor) {
 		super.visitInsns(visitor);
-		if (condition != null) {
-			Nullability.castToNonnull(condition).visitInsns(visitor);
-		}
+		condition.visitInsns(visitor);
 	}
 
 	@Override
@@ -84,13 +75,7 @@ public final class TernaryInsn extends InsnNode {
 			return false;
 		}
 		TernaryInsn that = (TernaryInsn) obj;
-		if (condition == null) {
-			return that.condition == null;
-		}
-		if (that.condition == null) {
-			return false;
-		}
-		return Nullability.castToNonnull(condition).equals(that.condition);
+		return condition.equals(that.condition);
 	}
 
 	@Override
@@ -103,13 +88,10 @@ public final class TernaryInsn extends InsnNode {
 	@Override
 	public void rebindArgs() {
 		super.rebindArgs();
-		IfCondition cond = condition;
-		if (cond != null) {
-			for (RegisterArg reg : cond.getRegisterArgs()) {
-				InsnNode parentInsn = reg.getParentInsn();
-				if (parentInsn != null) {
-					parentInsn.rebindArgs();
-				}
+		for (RegisterArg reg : condition.getRegisterArgs()) {
+			InsnNode parentInsn = reg.getParentInsn();
+			if (parentInsn != null) {
+				parentInsn.rebindArgs();
 			}
 		}
 	}
