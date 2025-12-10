fda44e36a43fb1a2547e2c21359a1f4ceb978641
Pascal-Joos <pascal.joos@outlook.de>
2025-12-10 10:31:32 +0000

    Revert "fix: 27 - METHOD_NO_INIT - private InvokeCustomNode(MethodInfo mth, InvokeType invokeType, int argsCount) { - initializer method does not guarantee @NonNull fields implMthInfo (line 13), callInsn (line 16) are initialized along all control-flow paths (remember to check for exceptions or early returns)."
    This reverts commit e660d66b8bf623e7048172389958be6052c61d9e.

--- jadx-core/src/main/java/jadx/core/dex/instructions/InvokeCustomNode.java
+++ jadx-core/src/main/java/jadx/core/dex/instructions/InvokeCustomNode.java
@@ -2,8 +2,6 @@ package jadx.core.dex.instructions;
 
 import org.jetbrains.annotations.Nullable;
 
-import edu.ucr.cs.riple.annotator.util.Nullability;
-
 import jadx.api.plugins.input.data.MethodHandleType;
 import jadx.api.plugins.input.insns.InsnData;
 import jadx.core.dex.info.MethodInfo;
@@ -12,11 +10,9 @@ import jadx.core.dex.nodes.InsnNode;
 import jadx.core.utils.InsnUtils;
 
 public class InvokeCustomNode extends InvokeNode {
-	@Nullable
 	private MethodInfo implMthInfo;
 	@Nullable
 	private MethodHandleType handleType;
-	@Nullable
 	private InsnNode callInsn;
 	private boolean inlineInsn;
 	private boolean useRef;
@@ -35,9 +31,7 @@ public class InvokeCustomNode extends InvokeNode {
 		copyCommonParams(copy);
 		copy.setImplMthInfo(implMthInfo);
 		copy.setHandleType(handleType);
-		if (callInsn != null) {
-			copy.setCallInsn(callInsn);
-		}
+		copy.setCallInsn(callInsn);
 		copy.setInlineInsn(inlineInsn);
 		copy.setUseRef(useRef);
 		return copy;
@@ -53,17 +47,12 @@ public class InvokeCustomNode extends InvokeNode {
 		}
 		InvokeCustomNode other = (InvokeCustomNode) obj;
 		return handleType == other.handleType
-				&& implMthInfo != null
-				&& other.implMthInfo != null
-				&& implMthInfo.equals(Nullability.castToNonnull(other.implMthInfo))
-				&& callInsn != null
-				&& other.callInsn != null
-				&& callInsn.isSame(Nullability.castToNonnull(other.callInsn))
+				&& implMthInfo.equals(other.implMthInfo)
+				&& callInsn.isSame(other.callInsn)
 				&& inlineInsn == other.inlineInsn
 				&& useRef == other.useRef;
 	}
 
-	@Nullable
 	public MethodInfo getImplMthInfo() {
 		return implMthInfo;
 	}
@@ -81,7 +70,6 @@ public class InvokeCustomNode extends InvokeNode {
 		this.handleType = handleType;
 	}
 
-	@Nullable
 	public InsnNode getCallInsn() {
 		return callInsn;
 	}
@@ -108,10 +96,6 @@ public class InvokeCustomNode extends InvokeNode {
 
 	@Nullable
 	public BaseInvokeNode getInvokeCall() {
-		InsnNode callInsn = this.callInsn;
-		if (callInsn == null) {
-			return null;
-		}
 		if (callInsn.getType() == InsnType.INVOKE) {
 			return (BaseInvokeNode) callInsn;
 		}
