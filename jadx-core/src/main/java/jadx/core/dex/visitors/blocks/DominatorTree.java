63faec1659f282bf6bed91b21e5552d9e5af3dc4
Pascal-Joos <pascal.joos@outlook.de>
2025-12-10 11:44:29 +0000

    Revert "fix: 43 - RETURN_NULLABLE - return idom; - returning @Nullable expression from method with @NonNull return type"
    This reverts commit 384272fbd687c686814f5de1a1d3320b92bc2524.

--- jadx-core/src/main/java/jadx/core/dex/visitors/blocks/DominatorTree.java
+++ jadx-core/src/main/java/jadx/core/dex/visitors/blocks/DominatorTree.java
@@ -6,8 +6,6 @@ import java.util.List;
 
 import org.jetbrains.annotations.NotNull;
 
-import edu.ucr.cs.riple.annotator.util.Nullability;
-
 import jadx.core.dex.nodes.BlockNode;
 import jadx.core.dex.nodes.MethodNode;
 import jadx.core.utils.BlockUtils;
@@ -146,7 +144,7 @@ public class DominatorTree {
 					BlockNode runner = pred;
 					while (runner != idom) {
 						addToDF(runner, block, blocksCount);
-						runner = Nullability.castToNonnull(runner.getIDom());
+						runner = runner.getIDom();
 					}
 				}
 			}
@@ -160,7 +158,7 @@ public class DominatorTree {
 	}
 
 	private static void addToDF(BlockNode block, BlockNode dfBlock, int blocksCount) {
-		BitSet df = Nullability.castToNonnull(block).getDomFrontier();
+		BitSet df = block.getDomFrontier();
 		if (df == null) {
 			df = new BitSet(blocksCount);
 			block.setDomFrontier(df);
