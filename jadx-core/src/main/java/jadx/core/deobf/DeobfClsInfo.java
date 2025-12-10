47882118de81c6dd090351109988fadd1038e91c
Pascal-Joos <pascal.joos@outlook.de>
2025-12-10 10:03:06 +0000

    fix: 18 - RETURN_NULLABLE - return result; - returning @Nullable expression from method with @NonNull return type

--- jadx-core/src/main/java/jadx/core/deobf/DeobfClsInfo.java
+++ jadx-core/src/main/java/jadx/core/deobf/DeobfClsInfo.java
@@ -1,10 +1,13 @@
 package jadx.core.deobf;
 
+import javax.annotation.Nullable;
+
 import jadx.core.dex.nodes.ClassNode;
 
 class DeobfClsInfo {
 	private final Deobfuscator deobfuscator;
 	private final ClassNode cls;
+	@Nullable
 	private final PackageNode pkg;
 	private final String alias;
 
@@ -33,13 +36,14 @@ class DeobfClsInfo {
 	}
 
 	public String getFullName() {
-		return pkg.getFullAlias() + Deobfuscator.CLASS_NAME_SEPARATOR + makeNameWithoutPkg();
+		return (pkg != null ? pkg.getFullAlias() : "") + Deobfuscator.CLASS_NAME_SEPARATOR + makeNameWithoutPkg();
 	}
 
 	public ClassNode getCls() {
 		return cls;
 	}
 
+	@Nullable
 	public PackageNode getPkg() {
 		return pkg;
 	}
