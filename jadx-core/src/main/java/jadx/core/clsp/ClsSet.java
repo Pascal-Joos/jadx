e5e9a9620140586dc7c04ca8fe0faa9cee4895f3
Pascal-Joos <pascal.joos@outlook.de>
2025-12-10 09:26:11 +0000

    Revert "fix: 2 - RETURN_NULLABLE - return cls; - returning @Nullable expression from method with @NonNull return type"
    This reverts commit 3b1bd7e8234e64e2e2e52fea1edfbb6e03a400c5.

--- jadx-core/src/main/java/jadx/core/clsp/ClsSet.java
+++ jadx-core/src/main/java/jadx/core/clsp/ClsSet.java
@@ -158,12 +158,10 @@ public class ClsSet {
 		return getCls(cls.getRawName(), names);
 	}
 
-	@Nullable
 	private static ClspClass getCls(ArgType clsType, Map<String, ClspClass> names) {
 		return getCls(clsType.getObject(), names);
 	}
 
-	@Nullable
 	private static ClspClass getCls(String fullName, Map<String, ClspClass> names) {
 		ClspClass cls = names.get(fullName);
 		if (cls == null) {
@@ -294,8 +292,7 @@ public class ClsSet {
 			}
 		} else if (argType.isGeneric()) {
 			out.writeByte(TypeEnum.GENERIC.ordinal());
-			ClspClass cls = getCls(argType, names);
-			out.writeInt(cls == null ? -1 : cls.getId());
+			out.writeInt(getCls(argType, names).getId());
 			writeArgTypesList(out, argType.getGenericTypes(), names);
 		} else if (argType.isGenericType()) {
 			out.writeByte(TypeEnum.GENERIC_TYPE_VARIABLE.ordinal());
@@ -303,8 +300,7 @@ public class ClsSet {
 			writeArgTypesList(out, argType.getExtendTypes(), names);
 		} else if (argType.isObject()) {
 			out.writeByte(TypeEnum.OBJECT.ordinal());
-			ClspClass cls = getCls(argType, names);
-			out.writeInt(cls == null ? -1 : cls.getId());
+			out.writeInt(getCls(argType, names).getId());
 		} else if (argType.isArray()) {
 			out.writeByte(TypeEnum.ARRAY.ordinal());
 			writeArgType(out, argType.getArrayElement(), names);
