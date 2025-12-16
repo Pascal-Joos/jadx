6888efcabe1c54f0c8abd6ac8f9d234ab27dcf24
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 17:52:13 +0000

    Revert "fix: 95 - DEREFERENCE_NULLABLE - return arg.getType(); - dereferenced expression arg is @Nullable"
    This reverts commit 3ed8fa7853c513002c1fe3ddae1b125510b04eaa.

--- jadx-core/src/main/java/jadx/core/dex/visitors/typeinference/TypeUpdateInfo.java
+++ jadx-core/src/main/java/jadx/core/dex/visitors/typeinference/TypeUpdateInfo.java
@@ -46,9 +46,6 @@ public class TypeUpdateInfo {
 	}
 
 	public ArgType getType(@Nullable InsnArg arg) {
-		if (arg == null) {
-			return null;
-		}
 		for (TypeUpdateEntry update : updates) {
 			if (update.getArg() == arg) {
 				return update.getType();
