514a887284bdd109ec704fcbf227ea2856f02bf6
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 17:28:21 +0000

    Revert "fix: 74 - DEREFERENCE_NULLABLE - if (valueOfMths.contains(callMth)) { - dereferenced expression valueOfMths is @Nullable"
    This reverts commit bc343c1542cb0485a4e25d8fac567b92339db68a.

--- jadx-core/src/main/java/jadx/core/dex/visitors/DeboxingVisitor.java
+++ jadx-core/src/main/java/jadx/core/dex/visitors/DeboxingVisitor.java
@@ -92,10 +92,7 @@ public class DeboxingVisitor extends AbstractVisitor {
 			return null;
 		}
 		MethodInfo callMth = insnNode.getCallMth();
-		if (callMth == null) {
-			return null;
-		}
-		if (valueOfMths != null && valueOfMths.contains(callMth)) {
+		if (valueOfMths.contains(callMth)) {
 			RegisterArg resArg = insnNode.getResult();
 			InsnArg arg = insnNode.getArg(0);
 			if (arg.isLiteral()) {
