b5da9d49f0694c3f34b012cbfb23406c5f7bcc22
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 16:51:55 +0000

    fix: 48 - ASSIGN_FIELD_NULLABLE - this.codeReader = null; - assigning @Nullable expression to @NonNull field

--- jadx-core/src/main/java/jadx/core/dex/nodes/MethodNode.java
+++ jadx-core/src/main/java/jadx/core/dex/nodes/MethodNode.java
@@ -91,7 +91,7 @@ public class MethodNode extends NotificationAttrNode implements IMethodDetails, 
 		ICodeReader codeReader = mthData.getCodeReader();
 		this.noCode = codeReader == null;
 		if (noCode) {
-			this.codeReader = null;
+			this.codeReader = ICodeReader.EMPTY;
 			this.insnsCount = 0;
 		} else {
 			this.codeReader = codeReader.copy();
