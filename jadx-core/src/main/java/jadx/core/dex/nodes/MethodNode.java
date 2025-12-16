67d5327e6b903f64704396051332690327837759
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 16:56:15 +0000

    fix: 51 - RETURN_NULLABLE - return region; - returning @Nullable expression from method with @NonNull return type

--- jadx-core/src/main/java/jadx/core/dex/nodes/MethodNode.java
+++ jadx-core/src/main/java/jadx/core/dex/nodes/MethodNode.java
@@ -536,6 +536,9 @@ public class MethodNode extends NotificationAttrNode implements IMethodDetails, 
 	}
 
 	public Region getRegion() {
+		if (region == null) {
+			throw new IllegalStateException("Region is not initialized");
+		}
 		return region;
 	}
 
