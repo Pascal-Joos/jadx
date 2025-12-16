35968e264d3fb995a596bc5cf16e623a1b5c104e
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 17:50:47 +0000

    Revert "fix: 94 - RETURN_NULLABLE - return constraints; - returning @Nullable expression from method with @NonNull return type"
    This reverts commit 047c110705ed7c75976cf48e66ae479f10f3062b.

--- jadx-core/src/main/java/jadx/core/dex/visitors/typeinference/TypeSearchVarInfo.java
+++ jadx-core/src/main/java/jadx/core/dex/visitors/typeinference/TypeSearchVarInfo.java
@@ -82,9 +82,6 @@ public class TypeSearchVarInfo {
 	}
 
 	public List<ITypeConstraint> getConstraints() {
-		if (constraints == null) {
-			return new ArrayList<>();
-		}
 		return constraints;
 	}
 
