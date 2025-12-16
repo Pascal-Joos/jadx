33dd95e1b57996b1b30ad1da50f3c9c92b391113
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 17:16:17 +0000

    Revert "fix: 66 - DEREFERENCE_NULLABLE - loopRegion.getHeader().getInstructions().add(0, varArgInsn); - dereferenced expression loopRegion.getHeader() is @Nullable"
    This reverts commit a30e12282ffcac252a74ea3c5743b4e49b0e554e.

--- jadx-core/src/main/java/jadx/core/dex/regions/loops/ForEachLoop.java
+++ jadx-core/src/main/java/jadx/core/dex/regions/loops/ForEachLoop.java
@@ -26,14 +26,8 @@ public final class ForEachLoop extends LoopType {
 	}
 
 	public void injectFakeInsns(LoopRegion loopRegion) {
-		BlockNode preHeader = loopRegion.getInfo().getPreHeader();
-		if (preHeader != null) {
-			preHeader.getInstructions().add(iterableArgInsn);
-		}
-		BlockNode header = loopRegion.getHeader();
-		if (header != null) {
-			header.getInstructions().add(0, varArgInsn);
-		}
+		loopRegion.getInfo().getPreHeader().getInstructions().add(iterableArgInsn);
+		loopRegion.getHeader().getInstructions().add(0, varArgInsn);
 	}
 
 	public RegisterArg getVarArg() {
