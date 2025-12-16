eff2f4f77ad85fcb86c4df1db579a1f8cccb75bf
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 18:04:15 +0000

    Revert "fix: 104 - DEREFERENCE_NULLABLE - makeDirs(path.toAbsolutePath().getParent().toFile()); - dereferenced expression path.toAbsolutePath().getParent() is @Nullable"
    This reverts commit 18f34ecf97288735ce87fb6bd8b92f8cdc769c1f.

--- jadx-core/src/main/java/jadx/core/utils/files/FileUtils.java
+++ jadx-core/src/main/java/jadx/core/utils/files/FileUtils.java
@@ -79,10 +79,7 @@ public class FileUtils {
 
 	public static void makeDirsForFile(Path path) {
 		if (path != null) {
-			Path parent = path.toAbsolutePath().getParent();
-			if (parent != null) {
-				makeDirs(parent.toFile());
-			}
+			makeDirs(path.toAbsolutePath().getParent().toFile());
 		}
 	}
 
