c97100ce50c985253f55ea40b786d3649c8dafbe
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 16:28:49 +0000

    fix: 31 - RETURN_NULLABLE - return null; - returning @Nullable expression from method with @NonNull return type

--- jadx-core/src/main/java/jadx/core/dex/instructions/args/ArgType.java
+++ jadx-core/src/main/java/jadx/core/dex/instructions/args/ArgType.java
@@ -576,7 +576,7 @@ public abstract class ArgType {
 	}
 
 	public PrimitiveType getPrimitiveType() {
-		return null;
+		throw new UnsupportedOperationException("Primitive type is not known for: " + this.getClass());
 	}
 
 	public boolean isPrimitive() {
