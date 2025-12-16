e3c2d50a36b3cc98817cd69ddd38c6ec32d86808
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 16:39:23 +0000

    Revert "fix: 38 - FIELD_NO_INIT - protected ArgType type; - @NonNull field type not initialized"
    This reverts commit 32194229cdbd1c9b04b0c7c0f32fa8e7f01479ea.

--- jadx-core/src/main/java/jadx/core/dex/instructions/args/Typed.java
+++ jadx-core/src/main/java/jadx/core/dex/instructions/args/Typed.java
@@ -4,7 +4,7 @@ import jadx.core.dex.attributes.AttrNode;
 
 public abstract class Typed extends AttrNode {
 
-	protected ArgType type = ArgType.UNKNOWN;
+	protected ArgType type;
 
 	public ArgType getType() {
 		return type;
