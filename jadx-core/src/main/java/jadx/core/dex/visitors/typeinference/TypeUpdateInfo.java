3ed8fa7853c513002c1fe3ddae1b125510b04eaa
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 17:52:11 +0000

    fix: 95 - DEREFERENCE_NULLABLE - return arg.getType(); - dereferenced expression arg is @Nullable

--- jadx-core/src/main/java/jadx/core/dex/visitors/typeinference/TypeUpdateInfo.java
+++ jadx-core/src/main/java/jadx/core/dex/visitors/typeinference/TypeUpdateInfo.java
@@ -46,6 +46,9 @@ public class TypeUpdateInfo {
 	}
 
 	public ArgType getType(@Nullable InsnArg arg) {
+		if (arg == null) {
+			return null;
+		}
 		for (TypeUpdateEntry update : updates) {
 			if (update.getArg() == arg) {
 				return update.getType();
