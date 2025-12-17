64737c8f0e2373edb82037ea545d0fc148665459
Pascal-Joos <pascal.joos@outlook.de>
2025-12-17 13:16:26 +0000

    fix: Type='METHOD_NO_INIT', message='initializer method does not guarantee @NonNull field cleanSuccessors (line 44) is initialized along all control-flow paths (remember to check for exceptions or early returns).' /home/vscode/nullness-benchmarks/jadx/jadx-core/src/main/java/jadx/core/dex/nodes/BlockNode.java:68 	public BlockNode(int cid, int id, int offset) {

--- jadx-core/src/main/java/jadx/core/dex/nodes/BlockNode.java
+++ jadx-core/src/main/java/jadx/core/dex/nodes/BlockNode.java
@@ -69,6 +69,7 @@ public final class BlockNode extends AttrNode implements IBlock, Comparable<Bloc
 		this.cid = cid;
 		this.id = id;
 		this.startOffset = offset;
+		this.cleanSuccessors = new ArrayList<>(3);
 	}
 
 	public int getCId() {
