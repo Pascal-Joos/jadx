4462e39ca910c91bd78329a5516e5b64fcfd1e67
Pascal-Joos <pascal.joos@outlook.de>
2025-12-17 13:19:42 +0000

    fix: Type='RETURN_NULLABLE', message='returning @Nullable expression from method with @NonNull return type' /home/vscode/nullness-benchmarks/jadx/jadx-core/src/main/java/jadx/core/dex/nodes/MethodNode.java:307 		return instructions;

--- jadx-core/src/main/java/jadx/core/dex/nodes/MethodNode.java
+++ jadx-core/src/main/java/jadx/core/dex/nodes/MethodNode.java
@@ -304,7 +304,7 @@ public class MethodNode extends NotificationAttrNode implements IMethodDetails, 
 	}
 
 	public InsnNode[] getInstructions() {
-		return instructions;
+		return instructions == null ? new InsnNode[0] : instructions;
 	}
 
 	public void unloadInsnArr() {
