55ce42128c6583445a05d024e6932bfb1a2f3b1e
Pascal-Joos <pascal.joos@outlook.de>
2025-12-17 13:25:35 +0000

    fix: Type='DEREFERENCE_NULLABLE', message='dereferenced expression preCondition is @Nullable' /home/vscode/nullness-benchmarks/jadx/jadx-core/src/main/java/jadx/core/dex/regions/loops/LoopRegion.java:81 		List<InsnNode> insns = preCondition.getInstructions();

--- jadx-core/src/main/java/jadx/core/dex/regions/loops/LoopRegion.java
+++ jadx-core/src/main/java/jadx/core/dex/regions/loops/LoopRegion.java
@@ -78,6 +78,9 @@ public final class LoopRegion extends ConditionRegion {
 	 * Check if pre-conditions can be inlined into loop condition
 	 */
 	public boolean checkPreCondition() {
+		if (preCondition == null) {
+			return false;
+		}
 		List<InsnNode> insns = preCondition.getInstructions();
 		if (insns.isEmpty()) {
 			return true;
