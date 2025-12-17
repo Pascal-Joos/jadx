93eeefef37bfba7a9075f63721596f06410d4281
Pascal-Joos <pascal.joos@outlook.de>
2025-12-17 13:29:09 +0000

    fix: Type='DEREFERENCE_NULLABLE', message='dereferenced expression regNums is @Nullable' /home/vscode/nullness-benchmarks/jadx/jadx-core/src/main/java/jadx/core/dex/visitors/InlineMethods.java:93 			for (int i = 0; i < regNums.length; i++) {

--- jadx-core/src/main/java/jadx/core/dex/visitors/InlineMethods.java
+++ jadx-core/src/main/java/jadx/core/dex/visitors/InlineMethods.java
@@ -95,6 +95,10 @@ public class InlineMethods extends AbstractVisitor {
 			// remap args
 			InsnArg[] regs = new InsnArg[callMth.getRegsCount()];
 			int[] regNums = mia.getArgsRegNums();
+			if (regNums == null) {
+				mth.addWarnComment("Failed to inline method (no args reg nums): " + callMth);
+				return;
+			}
 			for (int i = 0; i < regNums.length; i++) {
 				InsnArg arg = insn.getArg(i);
 				regs[regNums[i]] = arg;
