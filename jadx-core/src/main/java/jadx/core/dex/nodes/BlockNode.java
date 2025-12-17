3f9cc8efaf762ed4eea004fae8b51fe3efdb3728
Pascal-Joos <pascal.joos@outlook.de>
2025-12-17 13:17:13 +0000

    fix: Type='RETURN_NULLABLE', message='returning @Nullable expression from method with @NonNull return type' /home/vscode/nullness-benchmarks/jadx/jadx-core/src/main/java/jadx/core/dex/nodes/BlockNode.java:184 		return idom;

--- jadx-core/src/main/java/jadx/core/dex/nodes/BlockNode.java
+++ jadx-core/src/main/java/jadx/core/dex/nodes/BlockNode.java
@@ -185,6 +185,9 @@ public final class BlockNode extends AttrNode implements IBlock, Comparable<Bloc
 	 * Immediate dominator
 	 */
 	public BlockNode getIDom() {
+		if (idom == null) {
+			throw new IllegalStateException("idom is not initialized");
+		}
 		return idom;
 	}
 
