bb080b49189b844e6db41b689c35c2e8c9693436
Pascal-Joos <pascal.joos@outlook.de>
2026-01-08 18:20:58 +0000

    fix: 83 - RETURN_NULLABLE - return curDupInsns; - returning @Nullable expression from method with @NonNull return type

--- jadx-core/src/main/java/jadx/core/dex/visitors/finaly/MarkFinallyVisitor.java
+++ jadx-core/src/main/java/jadx/core/dex/visitors/finaly/MarkFinallyVisitor.java
@@ -9,6 +9,8 @@ import org.jetbrains.annotations.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import edu.ucr.cs.riple.annotator.util.Nullability;
+
 import jadx.core.Consts;
 import jadx.core.dex.attributes.AFlag;
 import jadx.core.dex.attributes.AType;
@@ -608,7 +610,8 @@ public class MarkFinallyVisitor extends AbstractVisitor {
 			return false;
 		}
 		List<InsnNode> curDupInsns = extractInfo.getCurDupInsns();
-		if (Utils.notEmpty(curDupInsns) && InsnList.contains(curDupInsns, dupAssign, extractInfo.getCurDupInsnsOffset())) {
+		if (Utils.notEmpty(curDupInsns)
+				&& InsnList.contains(Nullability.castToNonnull(curDupInsns), dupAssign, extractInfo.getCurDupInsnsOffset())) {
 			return false;
 		}
 		return true;
