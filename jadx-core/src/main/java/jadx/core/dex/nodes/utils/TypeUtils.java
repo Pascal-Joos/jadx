b9e5f09771a5dea1ce8d45f950fd3b17ce97bcef
Pascal-Joos <pascal.joos@outlook.de>
2026-01-08 15:44:43 +0000

    fix: 30 - RETURN_NULLABLE - return null; - returning @Nullable expression from method with @NonNull return type

--- jadx-core/src/main/java/jadx/core/dex/nodes/utils/TypeUtils.java
+++ jadx-core/src/main/java/jadx/core/dex/nodes/utils/TypeUtils.java
@@ -11,6 +11,8 @@ import java.util.function.BiConsumer;
 
 import org.jetbrains.annotations.Nullable;
 
+import edu.ucr.cs.riple.annotator.util.Nullability;
+
 import jadx.core.clsp.ClspClass;
 import jadx.core.dex.attributes.AType;
 import jadx.core.dex.attributes.nodes.ClassTypeVarsAttr;
@@ -280,7 +282,7 @@ public class TypeUtils {
 
 	@Nullable
 	public ArgType replaceTypeVariablesUsingMap(ArgType replaceType, Map<ArgType, ArgType> replaceMap) {
-		if (replaceMap.isEmpty()) {
+		if (replaceType == null || replaceMap.isEmpty()) {
 			return null;
 		}
 		if (replaceType.isGenericType()) {
@@ -310,8 +312,8 @@ public class TypeUtils {
 				if (replacedOuter == null) {
 					return null;
 				}
-				ArgType innerType = replaceType.getInnerType();
-				ArgType replacedInner = replaceTypeVariablesUsingMap(innerType, replaceMap);
+				ArgType innerType = Nullability.castToNonnull(replaceType.getInnerType());
+				ArgType replacedInner = replaceTypeVariablesUsingMap(Nullability.castToNonnull(innerType), replaceMap);
 				return ArgType.outerGeneric(replacedOuter, replacedInner == null ? innerType : replacedInner);
 			}
 			List<ArgType> genericTypes = replaceType.getGenericTypes();
