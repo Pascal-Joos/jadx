8973fa7991a45172e829bebfe46ad7c82300ea2d
Pascal-Joos <pascal.joos@outlook.de>
2026-01-08 14:30:46 +0000

    fix: 2 - RETURN_NULLABLE - return cls; - returning @Nullable expression from method with @NonNull return type

--- jadx-core/src/main/java/jadx/core/clsp/ClsSet.java
+++ jadx-core/src/main/java/jadx/core/clsp/ClsSet.java
@@ -27,6 +27,8 @@ import org.jetbrains.annotations.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import edu.ucr.cs.riple.annotator.util.Nullability;
+
 import jadx.api.plugins.utils.ZipSecurity;
 import jadx.core.dex.info.AccessInfo;
 import jadx.core.dex.info.ClassInfo;
@@ -158,10 +160,12 @@ public class ClsSet {
 		return getCls(cls.getRawName(), names);
 	}
 
+	@Nullable
 	private static ClspClass getCls(ArgType clsType, Map<String, ClspClass> names) {
 		return getCls(clsType.getObject(), names);
 	}
 
+	@Nullable
 	private static ClspClass getCls(String fullName, Map<String, ClspClass> names) {
 		ClspClass cls = names.get(fullName);
 		if (cls == null) {
@@ -292,7 +296,11 @@ public class ClsSet {
 			}
 		} else if (argType.isGeneric()) {
 			out.writeByte(TypeEnum.GENERIC.ordinal());
-			out.writeInt(getCls(argType, names).getId());
+			ClspClass cls = getCls(argType, names);
+			if (cls == null) {
+				throw new JadxRuntimeException("Cannot save type, class not found: " + argType);
+			}
+			out.writeInt(Nullability.castToNonnull(getCls(argType, names)).getId());
 			writeArgTypesList(out, argType.getGenericTypes(), names);
 		} else if (argType.isGenericType()) {
 			out.writeByte(TypeEnum.GENERIC_TYPE_VARIABLE.ordinal());
@@ -300,7 +308,11 @@ public class ClsSet {
 			writeArgTypesList(out, argType.getExtendTypes(), names);
 		} else if (argType.isObject()) {
 			out.writeByte(TypeEnum.OBJECT.ordinal());
-			out.writeInt(getCls(argType, names).getId());
+			ClspClass cls = getCls(argType, names);
+			if (cls == null) {
+				throw new JadxRuntimeException("Cannot save type, class not found: " + argType);
+			}
+			out.writeInt(cls.getId());
 		} else if (argType.isArray()) {
 			out.writeByte(TypeEnum.ARRAY.ordinal());
 			writeArgType(out, argType.getArrayElement(), names);
