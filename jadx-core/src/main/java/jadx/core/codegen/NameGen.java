fee045c16d16420f9adf6ed32c0aa060ff2529fa
Pascal-Joos <pascal.joos@outlook.de>
2026-01-08 16:02:41 +0000

    fix: 36 - RETURN_NULLABLE - return type; - returning @Nullable expression from method with @NonNull return type

--- jadx-core/src/main/java/jadx/core/codegen/NameGen.java
+++ jadx-core/src/main/java/jadx/core/codegen/NameGen.java
@@ -7,6 +7,8 @@ import java.util.Set;
 
 import javax.annotation.Nullable;
 
+import edu.ucr.cs.riple.annotator.util.Nullability;
+
 import jadx.core.Consts;
 import jadx.core.deobf.NameMapper;
 import jadx.core.dex.attributes.AFlag;
@@ -175,13 +177,13 @@ public class NameGen {
 	}
 
 	private String makeNameForType(ArgType type) {
-		if (type.isPrimitive()) {
-			return type.getPrimitiveType().getShortName().toLowerCase();
+		if (Nullability.castToNonnull(type).isPrimitive()) {
+			return Nullability.castToNonnull(type).getPrimitiveType().getShortName().toLowerCase();
 		}
-		if (type.isArray()) {
-			return makeNameForType(type.getArrayRootElement()) + "Arr";
+		if (Nullability.castToNonnull(type).isArray()) {
+			return makeNameForType(Nullability.castToNonnull(type).getArrayRootElement()) + "Arr";
 		}
-		return makeNameForObject(type);
+		return makeNameForObject(Nullability.castToNonnull(type));
 	}
 
 	private String makeNameForObject(ArgType type) {
