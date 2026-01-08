83a3271e431e5cb04cc4c22e69db6c24e132a5a0
Pascal-Joos <pascal.joos@outlook.de>
2026-01-08 14:35:40 +0000

    Revert "fix: 4 - RETURN_NULLABLE - return parents; - returning @Nullable expression from method with @NonNull return type"
    This reverts commit 1ecd76f984b4e2096aecb8a162dede1af4d40b8c.

--- jadx-core/src/main/java/jadx/core/dex/visitors/OverrideMethodVisitor.java
+++ jadx-core/src/main/java/jadx/core/dex/visitors/OverrideMethodVisitor.java
@@ -301,15 +301,12 @@ public class OverrideMethodVisitor extends AbstractVisitor {
 		}
 		ClspClass clsDetails = root.getClsp().getClsDetails(superType);
 		if (clsDetails != null) {
-			ArgType[] parents = clsDetails.getParents();
-			if (parents != null) {
-				int k = 0;
-				for (ArgType parentType : parents) {
-					k += addSuperType(root, superTypesMap, endTypes, parentType);
-				}
-				if (k == 0) {
-					endTypes.add(superType.getObject());
-				}
+			int k = 0;
+			for (ArgType parentType : clsDetails.getParents()) {
+				k += addSuperType(root, superTypesMap, endTypes, parentType);
+			}
+			if (k == 0) {
+				endTypes.add(superType.getObject());
 			}
 			return 1;
 		}
