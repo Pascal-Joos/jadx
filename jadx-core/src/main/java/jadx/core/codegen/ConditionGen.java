8944e11046b673290a55b5b30acf68bd5e11e218
Pascal-Joos <pascal.joos@outlook.de>
2026-01-08 14:44:32 +0000

    fix: 9 - DEREFERENCE_NULLABLE - switch (condition.getMode()) { - dereferenced expression condition is @Nullable

--- jadx-core/src/main/java/jadx/core/codegen/ConditionGen.java
+++ jadx-core/src/main/java/jadx/core/codegen/ConditionGen.java
@@ -6,6 +6,8 @@ import java.util.Queue;
 
 import javax.annotation.Nullable;
 
+import edu.ucr.cs.riple.annotator.util.Nullability;
+
 import jadx.api.ICodeWriter;
 import jadx.core.dex.attributes.AFlag;
 import jadx.core.dex.instructions.ArithNode;
@@ -54,26 +56,26 @@ public class ConditionGen extends InsnGen {
 
 	private void add(ICodeWriter code, CondStack stack, @Nullable IfCondition condition) throws CodegenException {
 		stack.push(condition);
-		switch (condition.getMode()) {
+		switch (Nullability.castToNonnull(condition).getMode()) {
 			case COMPARE:
-				addCompare(code, stack, condition.getCompare());
+				addCompare(code, stack, Nullability.castToNonnull(condition).getCompare());
 				break;
 
 			case TERNARY:
-				addTernary(code, stack, condition);
+				addTernary(code, stack, Nullability.castToNonnull(condition));
 				break;
 
 			case NOT:
-				addNot(code, stack, condition);
+				addNot(code, stack, Nullability.castToNonnull(condition));
 				break;
 
 			case AND:
 			case OR:
-				addAndOr(code, stack, condition);
+				addAndOr(code, stack, Nullability.castToNonnull(condition));
 				break;
 
 			default:
-				throw new JadxRuntimeException("Unknown condition mode: " + condition.getMode());
+				throw new JadxRuntimeException("Unknown condition mode: " + Nullability.castToNonnull(condition).getMode());
 		}
 		stack.pop();
 	}
