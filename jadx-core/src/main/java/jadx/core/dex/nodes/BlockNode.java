7ea1d724ade181a5280cd7aacf2918ee757a8973
Pascal-Joos <pascal.joos@outlook.de>
2026-01-08 16:25:31 +0000

    fix: 44 - ASSIGN_FIELD_NULLABLE - this.doms = doms; - assigning @Nullable expression to @NonNull field

--- jadx-core/src/main/java/jadx/core/dex/nodes/BlockNode.java
+++ jadx-core/src/main/java/jadx/core/dex/nodes/BlockNode.java
@@ -46,6 +46,7 @@ public final class BlockNode extends AttrNode implements IBlock, Comparable<Bloc
 	/**
 	 * All dominators, excluding self
 	 */
+	@Nullable
 	private BitSet doms = EmptyBitSet.EMPTY;
 
 	/**
@@ -155,12 +156,13 @@ public final class BlockNode extends AttrNode implements IBlock, Comparable<Bloc
 	 * Check if 'block' dominated on this node
 	 */
 	public boolean isDominator(BlockNode block) {
-		return doms.get(block.getId());
+		return doms != null && doms.get(block.getId());
 	}
 
 	/**
 	 * Dominators of this node (exclude itself)
 	 */
+	@Nullable
 	public BitSet getDoms() {
 		return doms;
 	}
