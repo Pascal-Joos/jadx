816a2f8406ce702cc71309b59e9c691dbc5901d7
Pascal-Joos <pascal.joos@outlook.de>
2026-01-08 19:19:17 +0000

    Revert "fix: 110 - METHOD_NO_INIT - public ProtoXMLParser(RootNode rootNode) { - initializer method does not guarantee @NonNull fields nsMap (line 23), writer (line 26) are initialized along all control-flow paths (remember to check for exceptions or early returns)."
    This reverts commit 675d842e9b3ec1f4d5a2c309b46715681772c8c7.

--- jadx-core/src/main/java/jadx/core/xmlgen/ProtoXMLParser.java
+++ jadx-core/src/main/java/jadx/core/xmlgen/ProtoXMLParser.java
@@ -14,19 +14,15 @@ import com.android.aapt.Resources.XmlNamespace;
 import com.android.aapt.Resources.XmlNode;
 import com.google.protobuf.InvalidProtocolBufferException;
 
-import edu.ucr.cs.riple.annotator.util.Nullability;
-
 import jadx.api.ICodeInfo;
 import jadx.api.ICodeWriter;
 import jadx.core.dex.nodes.RootNode;
 import jadx.core.utils.StringUtils;
 
 public class ProtoXMLParser {
-	@Nullable
 	private Map<String, String> nsMap;
 	private final Map<String, String> tagAttrDeobfNames = new HashMap<>();
 
-	@Nullable
 	private ICodeWriter writer;
 
 	private final RootNode rootNode;
@@ -49,10 +45,7 @@ public class ProtoXMLParser {
 	}
 
 	private void decode(XmlNode n) throws IOException {
-		if (writer == null) {
-			writer = rootNode.makeCodeWriter();
-		}
-		if (writer != null && n.hasSource()) {
+		if (n.hasSource()) {
 			writer.attachSourceLine(n.getSource().getLineNumber());
 		}
 		writer.add(StringUtils.escapeXML(n.getText().trim()));
@@ -62,13 +55,10 @@ public class ProtoXMLParser {
 	}
 
 	private void decode(XmlElement e) throws IOException {
-		if (writer == null) {
-			return;
-		}
 		String tag = deobfClassName(e.getName());
 		tag = getValidTagAttributeName(tag);
 		currentTag = tag;
-		Nullability.castToNonnull(writer).startLine('<').add(tag);
+		writer.startLine('<').add(tag);
 		for (int i = 0; i < e.getNamespaceDeclarationCount(); i++) {
 			decode(e.getNamespaceDeclaration(i));
 		}
@@ -76,27 +66,24 @@ public class ProtoXMLParser {
 			decode(e.getAttribute(i));
 		}
 		if (e.getChildCount() > 0) {
-			Nullability.castToNonnull(writer).add('>');
-			Nullability.castToNonnull(writer).incIndent();
+			writer.add('>');
+			writer.incIndent();
 			for (int i = 0; i < e.getChildCount(); i++) {
 				Map<String, String> oldNsMap = new HashMap<>(nsMap);
 				decode(e.getChild(i));
 				nsMap = oldNsMap;
 			}
-			Nullability.castToNonnull(writer).decIndent();
-			Nullability.castToNonnull(writer).startLine("</").add(tag).add('>');
+			writer.decIndent();
+			writer.startLine("</").add(tag).add('>');
 		} else {
-			Nullability.castToNonnull(writer).add("/>");
+			writer.add("/>");
 		}
 	}
 
 	private void decode(XmlAttribute a) {
-		if (writer == null) {
-			return;
-		}
 		writer.add(' ');
 		String namespace = a.getNamespaceUri();
-		if (nsMap != null && !namespace.isEmpty()) {
+		if (!namespace.isEmpty()) {
 			writer.add(nsMap.get(namespace)).add(':');
 		}
 		String name = a.getName();
@@ -106,13 +93,10 @@ public class ProtoXMLParser {
 	}
 
 	private void decode(XmlNamespace n) {
-		if (nsMap == null || writer == null) {
-			return;
-		}
 		String prefix = n.getPrefix();
 		String uri = n.getUri();
 		nsMap.put(uri, prefix);
-		Nullability.castToNonnull(writer).add(" xmlns:").add(prefix).add("=\"").add(uri).add('"');
+		writer.add(" xmlns:").add(prefix).add("=\"").add(uri).add('"');
 	}
 
 	private void memorizePackageName(String attrName, String attrValue) {
