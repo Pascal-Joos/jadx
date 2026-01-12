04fb729f2e9cb350940f7640ece41e7a61b65055
Pascal-Joos <pascal.joos@outlook.de>
2026-01-12 12:25:05 +0000

    fix: Type='FIELD_NO_INIT', message='@NonNull field TemplateFile$ParserState.curVariable not initialized' /home/vscode/nullness-benchmarks/jadx/jadx-core/src/main/java/jadx/core/export/TemplateFile.java:31 		private StringBuilder curVariable;

--- jadx-core/src/main/java/jadx/core/export/TemplateFile.java
+++ jadx-core/src/main/java/jadx/core/export/TemplateFile.java
@@ -28,6 +28,7 @@ public class TemplateFile {
 
 	private static class ParserState {
 		private State state = State.NONE;
+		@Nullable
 		private StringBuilder curVariable;
 		private boolean skip;
 	}
@@ -88,6 +89,7 @@ public class TemplateFile {
 	}
 
 	@Nullable
+	@javax.annotation.Nonnull
 	private String process(ParserState parser, char ch) {
 		State state = parser.state;
 		switch (ch) {
@@ -114,6 +116,9 @@ public class TemplateFile {
 
 					case END:
 						parser.state = State.NONE;
+						if (parser.curVariable == null) {
+							return null;
+						}
 						String varName = parser.curVariable.toString();
 						parser.curVariable = new StringBuilder();
 						return processVar(varName);
@@ -123,6 +128,9 @@ public class TemplateFile {
 			default:
 				switch (state) {
 					case VARIABLE:
+						if (parser.curVariable == null) {
+							return null;
+						}
 						parser.curVariable.append(ch);
 						parser.skip = true;
 						return null;
